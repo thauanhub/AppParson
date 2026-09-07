96ce346c1d71c220f87c2592a91ef397bc1ac6da
Thauan <thauanrocha123@gmail.com>
2026-09-07 10:32:00 -0300

    feat: admin parsons_faded

---
+++ ic/parsons_faded/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import ProblemPF, SolutionPF
+
+# Register your models here.
+admin.site.register(ProblemPF)
+admin.site.register(SolutionPF)
--- ic/parsons_faded/views.py
+++ ic/parsons_faded/views.py
@@ -5,6 +5,10 @@ from django.shortcuts import redirect, render
 
 from .problems import PROBLEMAS, gabarito_de, get_problema_ou_404
 
+from django.shortcuts import render, get_object_or_404, redirect
+
+from .models import ProblemPF, SolutionPF
+
 
 def parsons_faded_home(request):
     """Raiz do protótipo: leva direto ao primeiro exemplo."""
@@ -20,18 +24,19 @@ def parsons_faded_problem(request, problem_id):
     linha (option) tem um id, a indentação esperada e partes (texto fixo ou
     trecho apagado a ser preenchido).
     """
-    problema = get_problema_ou_404(problem_id)
+    problema = get_object_or_404(ProblemPF.objects.filter(question_type='F'), id=problem_id)
 
-    linhas = problema['options']
-    linhas_embaralhadas = linhas[:]
-    random.shuffle(linhas_embaralhadas)
+    # linhas = problema['options']
+    linhas = problema.options.splitlines()
+    linhas_embaralhadas = random.shuffle(linhas)
+    # random.shuffle(linhas_embaralhadas)
 
     context = {
         'title': 'Parsons Faded',
         'problema': problema,
         'linhas_embaralhadas': linhas_embaralhadas,
-        'qtd_fades': sum(linha['tem_fade'] for linha in linhas),
-        'gabarito_json': json.dumps(gabarito_de(linhas)),
-        'problemas_disponiveis': PROBLEMAS,
+        # 'qtd_fades': sum(linha['tem_fade'] for linha in linhas),
+        'gabarito_json': json.dumps(linhas),
+        'problemas_disponiveis': ProblemPF.objects.filter(question_type='F').order_by('id'),
     }
     return render(request, 'parsons_faded.html', context)
